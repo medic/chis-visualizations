c70bb781cb7ff771a6e54621ee769787e9ba7808
Anand <anand.narasimhamurth@accenture.com>
2021-03-25 14:49:50 +0530

    Changes to read data of metadata from config and removed hardcoding..

--- staginng_to_dw/main.py
+++ staginng_to_dw/main.py
@@ -3,6 +3,7 @@ import psycopg2
 from config import config
 import helper
 import pandas as pd
+import json
 
 
 def fetch_metadata():
@@ -11,25 +12,37 @@ def fetch_metadata():
     try:
         # read connection parameters
         params = config()
-
+        # read metadata config file to get the schema name and table names of the metadata config
+        metadata_file = open('metadata_config.json', "r")
+        metadata = json.loads(metadata_file.read())
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
 
         # create a cursor
         cur = conn.cursor()
+        # Iterating through the json
+        # list
+        if metadata['source_details_schema_name'] == "":
+            source_details_query = "select * from " + metadata['source_details_table_name'] + " where processing_needed = 'yes' "
+        else:
+            source_details_query = "select * from " + metadata['source_details_schema_name']+"."+metadata['source_details_table_name'] + " where processing_needed = 'yes' "
+        print('source_details_query', source_details_query)
 
-        # execute a statement
-        cur.execute("SELECT * from test_db.source_details where processing_needed = 'yes'")
-
-        # display the PostgreSQL database server version
-        metadata_details = cur.fetchall()
+        cur.execute(source_details_query)
+        metadata_details_data = cur.fetchall()
 
         column_name = [entry.name for entry in cur.description]
 
         metadata_mapping_details = []
-        for run_id in metadata_details:
-            cur.execute("SELECT * from test_db.source_mapping_details where run_id = run_id")
+        for data in metadata_details_data:
+            print('processing for run id',str(data[0]))
+            if metadata['mapping_schema_name'] == "":
+                mapping_details_query = "select * from " + metadata['mapping_table_name'] + " where run_id = "+str(data[0])
+            else:
+                mapping_details_query = "select * from " + metadata['mapping_schema_name'] + "." + metadata['mapping_table_name'] + " where run_id = "+str(data[0])
+            print('mapping_details_query',mapping_details_query)
+            cur.execute(mapping_details_query)
             for entry in cur.fetchall():
                 metadata_mapping_details.append(list(entry))
 
@@ -37,7 +50,7 @@ def fetch_metadata():
         # close the communication with the PostgreSQL
         cur.close()
 
-        return pd.DataFrame(data=metadata_details, columns=column_name), \
+        return pd.DataFrame(data=metadata_details_data, columns=column_name), \
                pd.DataFrame(data=metadata_mapping_details, columns=mapping_column)
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
